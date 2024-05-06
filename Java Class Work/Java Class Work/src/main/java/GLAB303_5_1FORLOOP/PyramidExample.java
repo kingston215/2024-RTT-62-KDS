63d57ed200ad13b3640917265e4e85c5eb4396c1
Kenneth Smith <smithkenneth15@yahoo.com>
2024-05-05 20:28:26 -0400

    Guided labs practice homework

---
+++ Java Class Work/Java Class Work/src/main/java/GLAB303_5_1FORLOOP/PyramidExample.java
@@ -0,0 +1,13 @@
+package GLAB303_5_1FORLOOP;
+
+public class PyramidExample {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();//new line
+        }
+    }
+}
+
---
+++ Java Class Work/Java Class Work/src/main/java/GLAB303_5_1FORLOOP/loopdemoone.java
@@ -0,0 +1,13 @@
+package GLAB303_5_1FORLOOP;
+
+import java.util.Scanner;
+
+public class loopdemoone {
+    public static void main(String[] args) {
+        int n = 5;
+        // for loop
+        for (int i = 1; i <= n; ++i) {
+            System.out.println("Java is fun");
+        }
+    }
+}
