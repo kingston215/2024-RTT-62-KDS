252ea796179f1249e76e278a38864034dab76662
Kenneth Smith <smithkenneth15@yahoo.com>
2024-05-10 23:13:21 -0400

    GLAB 303.11.1

---
+++ Java Class Work/Java Class Work/src/main/java/GLAB_303_11_1/DemoClass.java
@@ -0,0 +1,14 @@
+package GLAB_303_11_1;
+
+class DemoClass {
+
+    // create a generics method
+    public <T> void genericsMethod(T data) {
+        System.out.println("Generics Method:");
+        System.out.println("Data Passed: " + data);
+    }
+}
+
+
+
+
