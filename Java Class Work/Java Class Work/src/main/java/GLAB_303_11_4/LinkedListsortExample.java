a90cd4f2c39ec10ce4d12a253fc5903e6aa59fe1
Kenneth Smith <smithkenneth15@yahoo.com>
2024-05-11 00:33:23 -0400

    GLAB 303.11.4 LinkedList Processing

---
+++ Java Class Work/Java Class Work/src/main/java/GLAB_303_11_4/LinkedListsortExample.java
@@ -0,0 +1,33 @@
+package GLAB_303_11_4;
+
+
+import java.util.Collections;
+import java.util.List;
+
+public class LinkedListsortExample {
+    public static <LinkedList extends List<T>, T> void main(String[] args)
+    {
+        //Create linked list
+        LinkedList linkedList = new LinkedList();
+        linkedList.add("A");
+        linkedList.add("C");
+        linkedList.add((T) "B");
+        linkedList.add((T) "D");
+
+//Unsorted
+        System.out.println(linkedList);
+
+//1. Sort the list
+        Collections.sort(linkedList);
+
+//Sorted
+        System.out.println(linkedList);
+
+//2. Custom sorting
+        linkedList.sort(Collections.reverseOrder());
+
+//Custom sorted
+        System.out.println(linkedList);
+    }
+}
+
