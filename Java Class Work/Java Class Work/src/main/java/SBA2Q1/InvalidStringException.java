3a7e335d44caca8d7b90bf3ef4405293c135edcd
Kenneth Smith <smithkenneth15@yahoo.com>
2024-05-21 08:40:00 -0400

    SBA practice question one.

---
+++ Java Class Work/Java Class Work/src/main/java/SBA2Q1/InvalidStringException.java
@@ -0,0 +1,17 @@
+package SBA2Q1;
+
+
+
+public class InvalidStringException extends Exception{
+
+
+    //SBA will specify the class names***
+    public InvalidStringException(String message) {
+
+        //the super keyword passes the message to the constructor of the parent
+        super(message);
+    }
+
+
+
+}
